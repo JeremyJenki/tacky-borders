0cfd4e8631906b2e6d6aec084d0e73400e8ecb25
Jenki <131179191+JeremyJenki@users.noreply.github.com>
2026-05-01 08:05:49 +0930

    Add files via upload

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("src/resources/icon.ico");
+    res.set("FileDescription", "Tacky Borders");
+    res.set("ProductName", "Tacky Borders");
     res.compile().unwrap();
 }
